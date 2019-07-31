be6fac84f311aeda31eb8ba64ec97f1a81c5d613
Nikita <leonov2424@mail.ru>
2019-08-01 00:17:06 +0400

    pytest. It is just simpler

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,12 +1,10 @@
-import unittest
-
 import requests
 
-class TestIndex(unittest.TestCase):
+
+class TestIndex:
     def test_connection(self):
         requests.get('http://localhost:80')
 
     def test_index(self):
         request_result = requests.get('http://127.0.0.1:5000')
-        self.assertEqual(request_result.content, b'Hello world!', 'Something went wrong.')
-
+        assert request_result.content == b'Hello world!', 'Something went wrong.'
