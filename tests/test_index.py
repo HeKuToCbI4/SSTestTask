9e0a44e1d0c920cbf543cd4b7e24ad44ee86d172
Nikita <leonov2424@mail.ru>
2019-08-01 00:17:06 +0400

    pytest. It is just simpler

--- tests/test_index.py
+++ tests/test_index.py
@@ -3,7 +3,7 @@ import requests
 
 class TestIndex:
     def test_connection(self):
-        requests.get('http://localhost:80')
+        requests.get('http://192.168.0.106:80')
 
     def test_index(self):
         request_result = requests.get('http://127.0.0.1:5000')
