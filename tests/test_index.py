53b189a9f8be35b03ca3cf50541ff66913daafdc
Nikita <leonov2424@mail.ru>
2019-08-01 00:28:42 +0400

    Changed. port.

--- tests/test_index.py
+++ tests/test_index.py
@@ -6,5 +6,5 @@ class TestIndex:
         requests.get('http://192.168.0.106:80')
 
     def test_index(self):
-        request_result = requests.get('http://127.0.0.1:5000')
+        request_result = requests.get('http://192.168.0.106:80')
         assert request_result.content == b'Hello world!', 'Something went wrong.'
